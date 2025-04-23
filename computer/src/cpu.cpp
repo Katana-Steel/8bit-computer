ca9c453f137015603c1e3bd60e0fd49a634fde43
Rene Kjellerup <rk.katana.steel@gmail.com>
2025-04-23 11:31:08 -0700

    added a memory addressing bit to the opcode byte
    since we only have 8 GP registers we only need the bottom 3 bits
    for the register index which allows the 4th bit to let it know to
    fetct the 2 byte address from memory.

--- computer/src/cpu.cpp
+++ computer/src/cpu.cpp
@@ -11,54 +11,66 @@ CPU::CPU()
 void CPU::executeInstruction(uint8_t instruction) {
   // Decode the instruction
   uint8_t opcode = (instruction & 0xF0) >> 4; // Extract the upper 4 bits
-  uint8_t regIndex = (instruction & 0x0F);    // Extract the lower 4 bits
+  uint8_t regIndex = (instruction & 0x07);    // Extract the lower 3 bits
+  bool memAddressing =
+      (instruction & 0x08); // Check if the instruction is memory addressing
+  uint16_t address = (mainMem->read(pc + 1) << 8) |
+                     mainMem->read(pc + 2); // pre-fetch a 16-bit address
 
   switch (opcode) {
   case 0x1: // Load value into a register
-    if (regIndex < 8) {
+    if (!memAddressing) {
       uint8_t value = mainMem->read(pc + 1); // Fetch the next byte as the value
       registers[regIndex] = value;
       pc++; // Increment program counter to account for the value byte
     } else {
-      uint16_t address = (mainMem->read(pc + 1) << 8) | mainMem->read(pc + 2);
-      registers[regIndex & 0x7] = mainMem->read(address);
+      registers[regIndex] = mainMem->read(address);
       pc += 2; // Increment program counter to account for the 2-byte address
     }
     break;
   case 0x2: // Store value from a register to memory
-    if (regIndex < 8) {
-      uint16_t address = (mainMem->read(pc + 1) << 8) |
-                         mainMem->read(pc + 2); // Fetch 16-bit address
-      mainMem->write(
-          address,
-          registers[regIndex]); // Store the register value at the address
+    mainMem->write(
+        address,
+        registers[regIndex]); // Store the register value at the address
+    pc += 2; // Increment program counter to account for the 2-byte address
+    break;
+  case 0x3: { // Add a register to register[0]
+    int16_t tmp = 0;
+    if (memAddressing) {
+      tmp = (registers[regIndex] + mainMem->read(address));
       pc += 2; // Increment program counter to account for the 2-byte address
+    } else {
+      // Add the value of the specified register to register[0]
+      tmp = registers[0] + registers[regIndex];
     }
+    registers[0] = tmp & 0xFF; // Add the value of the specified
+    // register to register[0]
+    updateStatusFlags(tmp); // Update status flags (e.g., zero, carry, overflow)
     break;
-  case 0x3: // Add a register to register[0]
-    if (regIndex < 8) {
-      int16_t tmp = registers[0] + registers[regIndex];
-      registers[0] = tmp & 0xFF; // Add the value of the specified
-                                 // register to register[0]
-      updateStatusFlags(
-          tmp); // Update status flags (e.g., zero, carry, overflow)
+  }
+  case 0x4: { // Subtract a register from register[0]
+    int16_t tmp = 0;
+    if (memAddressing) {
+      tmp = (registers[regIndex] - mainMem->read(address));
+      pc += 2; // Increment program counter to account for the 2-byte address
+    } else {
+      // Add the value of the specified register to register[0]
+      tmp = registers[0] - registers[regIndex];
     }
+    registers[0] = tmp & 0xFF; // Add the value of the specified
+    // register to register[0]
+    updateStatusFlags(tmp); // Update status flags (e.g., zero, carry, overflow)
     break;
-  case 0x4: // Subtract a register from register[0]
-    if (regIndex < 8) {
-      int16_t tmp = registers[0] - registers[regIndex];
-      registers[0] -= tmp & 0xFF;
-      // from register[0]
-      updateStatusFlags(
-          tmp); // Update status flags (e.g., zero, carry, overflow)
-    }
-    break;
+  }
   case 0x5: // Jump to a specific memory address
   {
-    uint16_t address = (mainMem->read(pc + 1) << 8) |
-                       mainMem->read(pc + 2); // Fetch 16-bit address
-    address--; // subtract 1 because pc will be incremented later if set.
-    pc += 2;   // pre-Skip the address bytes if the jump is not taken
+    // Don't rely on unsigned wrap-around for address
+    if (address == 0) {
+      address = 0xFFFF; // Handle the case where address is 0
+    } else {
+      address--; // subtract 1 because pc will be incremented later if set.
+    }
+    pc += 2; // pre-Skip the address bytes if the jump is not taken
     switch (regIndex) {
     default: // Unconditional jump
       pc = address;
@@ -75,31 +87,46 @@ void CPU::executeInstruction(uint8_t instruction) {
       if (statusFlags & 0x03)
         pc = address;
       break;
+    case 4: // Jump on Not Zero
+      if ((statusFlags & 0x01)==0)
+        pc = address;
+      break;
     }
-
+    if (pc == address)
+      statusFlags = 0; // Clear the status flags if the jump is taken
   } break;
-  case 0x6: // Jump if Zero (JZ)
-  {
-    uint16_t address = (mainMem->read(pc + 1) << 8) |
-                       mainMem->read(pc + 2); // Fetch 16-bit address
-    if (statusFlags & 0x01) { // Check if the zero flag (bit 0) is set
-      pc = address - 1; // Set the program counter to the address (subtract 1
-                        // because pc will be incremented later)
+  case 0x6: // OR two registers and store the result in register[0]
+    if (!memAddressing) {
+      registers[0] |= registers[regIndex]; // Perform OR operation
     } else {
-      pc += 2; // Skip the address bytes if the jump is not taken
+      registers[0] = (registers[regIndex] | mainMem->read(address));
+      pc += 2; // Increment program counter to account for the 2-byte address
     }
-    statusFlags = 0;
-  } break;
-
+    updateStatusFlags(
+        registers[0]); // Update status flags (e.g., zero, carry, overflow)
+    break;
   case 0x7: // NAND two registers and store the result in register[0]
-    if (regIndex < 8) {
+    if (!memAddressing) {
       registers[0] =
           ~(registers[0] & registers[regIndex]); // Perform NAND operation
-      updateStatusFlags(
-          registers[0]); // Update status flags (e.g., zero, carry, overflow)
+    } else {
+      registers[0] = ~(registers[regIndex] & mainMem->read(address));
+      pc += 2; // Increment program counter to account for the 2-byte address
     }
+    updateStatusFlags(
+        registers[0]); // Update status flags (e.g., zero, carry, overflow)
     break;
-
+  case 0x8: { // CMP two registers and set the status flags
+    int16_t tmp = 0;
+    if (!memAddressing) {
+      tmp = registers[0] - registers[regIndex]; // Perform CMP operation
+    } else {
+      tmp = mainMem->read(address) - registers[regIndex];
+      pc += 2; // Increment program counter to account for the 2-byte address
+    }
+    updateStatusFlags(tmp); // Update status flags (e.g., zero, carry, overflow)
+    break;
+  }
   default:
     // Handle unknown opcode
     break;
